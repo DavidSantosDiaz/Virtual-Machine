68e0cb6d5e77522a9b878571d192ca037fa9c33d
DavidSantosDiaz <david.santos.diaz.21@gmail.com>
2021-03-21 18:08:30 +0100

    More

--- potencia.asm
+++ potencia.asm
@@ -1,30 +1,24 @@
 .ver 1
 
 .data 25 ;dirección de inicio de los datos
-dw base 2
-dw exp 3
-dw aux 0
-dw aux2 1
+dw base 3
+dw exponente 3
+dw aux	0
+dw aux2 0 
 dw aux3 1
-dw aux4 1
-dw aux5 0 
 dw limite 0
+dw extra 0
 
 .code
-T:	cmp limite, base
-	beq D
-       	add base, limite
-        add aux3, aux4
-        cmp aux4, exp
-        beq T
-        cmp aux3, aux3
-        beq T
-        
-        
-        mov limite, aux5
-D:	cmp aux2,exp
-fin:	beq fin
-	add base, aux
-	add base, aux5
-	cmp aux3, aux3
-	beq D
+eti:    mov extra, aux2
+        add base,aux
+        add aux3,aux2
+	cmp aux2,aux2
+        beq fin
+        cmp aux3,aux3
+fin:    beq eti
+        add aux3, limite
+end:    cmp limite, exponente
+        beq end
+        cmp aux3, aux3 
+        beq eti                  
