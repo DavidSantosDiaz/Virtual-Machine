b1ff8166c8ab37837dd75587a983e1951387c9b1
DavidSantosDiaz <david.santos.diaz.21@gmail.com>
2021-03-21 20:25:26 +0100

    --

--- potencia.asm
+++ potencia.asm
@@ -2,15 +2,16 @@
 
 .data 25 ;dirección de inicio de los datos
 dw base 3
-dw exponente 3
-dw aux	0
+dw exponente 2
+dw resultado	0
 dw aux2 0 
 dw aux3 1
 dw limite 0
-dw extra 1
+dw extra 0
 
 .code
-eti:	add base,aux
+rat:    mov limite, aux2
+eti:	add base,resultado
         add aux3,aux2
 	cmp aux2,base
         beq fin
@@ -20,4 +21,4 @@ fin:    add aux3, extra
         cmp extra, exponente
 end:    beq end
         cmp extra, extra
-        beq eti                              
+        beq rat                             
