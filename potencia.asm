c9bb1a25dc9c21f2455231e6791aa00dd9e5bf54
DavidSantosDiaz <david.santos.diaz.21@gmail.com>
2021-03-21 20:52:14 +0100

    ----

--- potencia.asm
+++ potencia.asm
@@ -8,13 +8,20 @@ dw var2	0
 dw aux	0
 dw aux2 0 
 dw aux3 1
+dw limite 1
+dw cero 0
 
 .code
         mov base, var2
 eti:	add base,aux
         add aux3,aux2
 	cmp aux2,var2
-fin:    beq fin
+        beq fin
         cmp aux3,aux3
         beq eti
-                                                    
+fin:    add aux3, limite
+        mov cero, aux2
+        cmp limite, exponente
+end:    beq end
+        cmp limite, limite
+        beq eti                                                            
