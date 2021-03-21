bba279d5d49326d39ec4a153ce1eec608aed8600
DavidSantosDiaz <david.santos.diaz.21@gmail.com>
2021-03-21 21:23:01 +0100

    I think now it is correct...

--- potencia.asm
+++ potencia.asm
@@ -1,18 +1,20 @@
 .ver 1
 
 
-.data 15 ;dirección de inicio de los datos
-dw base	2
+.data 20 ;dirección de inicio de los datos
+dw base	3
 dw exponente 3
 dw var2	0
 dw aux	0
 dw aux2 0 
 dw aux3 1
-dw limite 1
+dw limite 0
 dw cero 0
+dw resultado 0
 
 .code
         mov base, var2
+        mov var2, resultado
 eti:	add base,aux
         add aux3,aux2
 	cmp aux2,var2
@@ -20,6 +22,7 @@ eti:	add base,aux
         cmp aux3,aux3
         beq eti
 fin:    add aux3, limite
+        mov aux, resultado
         mov cero, aux2
         cmp limite, exponente
 end:    beq end
